84d39dd0ab36ffd3545aca29bb36c0fdbfe077f8
Kriss Rauhvargers <kriss.rauhvargers@lu.lv>
2012-02-08 22:41:39 +0200

    Test

--- application/config/doctypes.php
+++ application/config/doctypes.php
@@ -1,5 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
  
+//test
 $_doctypes = array(
 					'xhtml11'		=> '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">',
 					'xhtml1-strict'	=> '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">',
